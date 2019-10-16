285b2d91234a9ce713f9da047c98adb9084b9c38
Cristian Cereseto <cristiancereseto@gmail.com>
2019-10-16 15:16:56 -0300

    Added redux persist in order to save game on refresh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from '../src/redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from '../src/redux/store';
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,24 +15,26 @@ import Board from './containers/Board/Board';
 const App = () => {
   return (
       <Provider store={store}>
-        <div className="App">
-          <header className="App__header">
-            <img src={logo} className="App__logo" alt="logo" />
-            <p>
-              <code>MINE SWEEPER</code>
-            </p>
-          </header>
-          <Router>
-            <Switch>
-              <Route exact path="/">
-                <WelcomeForm />
-              </Route>
-              <Route exact path="/board">
-                <Board />
-              </Route>
-            </Switch>
-          </Router>
-        </div>
+        <PersistGate loading={null} persistor={persistor}>
+          <div className="App">
+            <header className="App__header">
+              <img src={logo} className="App__logo" alt="logo" />
+              <p>
+                <code>MINE SWEEPER</code>
+              </p>
+            </header>
+            <Router>
+              <Switch>
+                <Route exact path="/">
+                  <WelcomeForm />
+                </Route>
+                <Route exact path="/board">
+                  <Board />
+                </Route>
+              </Switch>
+            </Router>
+          </div>
+        </PersistGate>
       </Provider>
   );
 }
