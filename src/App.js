19575f3893d0802ed4ed64fbf2ff143b9736a80c
Cristian Cereseto <cristiancereseto@gmail.com>
2019-10-16 11:55:46 -0300

    move styles to sass, modify classes to bem

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App__header">
+        <img src={logo} className="App__logo" alt="logo" />
         <p>
           <code>MINE SWEEPER</code>
         </p>
+        <div className="App__welcome-form">
+          <label htmlFor="width">Width</label>
+          <input type="number" id="width"/>
+          <label htmlFor="height">Height</label>
+          <input type="number" id="height"/>
+          <label htmlFor="mines">Mines Amount</label>
+          <input type="number" id="mines"/>
+        </div>
         <a
-          className="App-link"
-          href="#"
-          rel="noopener noreferrer"
+            className="App__link"
+            href="#"
+            rel="noopener noreferrer"
         >
           Start
         </a>
